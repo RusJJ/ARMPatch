a6c2d3fb0afeb9900b3de61ae1f9f0f549835b42
RusJJ <mctesting1@yandex.ru>
2023-06-30 11:46:45 +0300

    Autobuilds

--- armpatch_src/Android.mk
+++ armpatch_src/Android.mk
@@ -2,15 +2,15 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := substrate
+LOCAL_CFLAGS := -O2 -mfloat-abi=softfp
 ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-	LOCAL_SRC_FILES := libsubstrate-armv7a_Cydia.a # Cydia Substrate
-	#LOCAL_SRC_FILES := libsubstrate-armv7a_Inline.a # Android Inline Hook by ele7enxxh (you can hook one function ONLY ONCE (hope im not wrong))
+    LOCAL_SRC_FILES := substrate/Hooker.cpp substrate/Debug.cpp substrate/PosixMemory.cpp # Cydia Substrate
 else
-	ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
-		LOCAL_SRC_FILES := libsubstrate-armv8a.a # And64InlineHook
-	endif
+    ifeq ($(TARGET_ARCH_ABI), arm64-v8a)
+        LOCAL_SRC_FILES := And64InlineHook/And64InlineHook.cpp # And64InlineHook
+    endif
 endif
-include $(PREBUILT_STATIC_LIBRARY)
+include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
@@ -21,13 +21,13 @@ LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG
 LOCAL_LDLIBS += -llog # ARM64 library requires for shared library (because that substrate was made with logs support)
 
  ## xDL ##
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/xdl/xdl/src/main/cpp $(LOCAL_PATH)/xdl/xdl/src/main/cpp/include
-LOCAL_SRC_FILES += xdl/xdl/src/main/cpp/xdl.c xdl/xdl/src/main/cpp/xdl_iterate.c \
-                   xdl/xdl/src/main/cpp/xdl_linker.c xdl/xdl/src/main/cpp/xdl_lzma.c \
-                   xdl/xdl/src/main/cpp/xdl_util.c
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/xDL/xdl/src/main/cpp $(LOCAL_PATH)/xDL/xdl/src/main/cpp/include
+LOCAL_SRC_FILES += xDL/xdl/src/main/cpp/xdl.c xDL/xdl/src/main/cpp/xdl_iterate.c \
+                   xDL/xdl/src/main/cpp/xdl_linker.c xDL/xdl/src/main/cpp/xdl_lzma.c \
+                   xDL/xdl/src/main/cpp/xdl_util.c
 LOCAL_CFLAGS += -D__XDL
 LOCAL_CXXFLAGS += -D__XDL
 
-include $(BUILD_SHARED_LIBRARY) # Will build it to .so library
-# include $(BUILD_STATIC_LIBRARY) # Will build it to static .a library <- it's better i think
-# include $(PREBUILT_SHARED_LIBRARY) # Can be used in LOCAL_SHARED_LIBRARIES <- much better
+include $(BUILD_STATIC_LIBRARY) # Will build it to static .a library
+# include $(PREBUILT_SHARED_LIBRARY) # Can be used in LOCAL_SHARED_LIBRARIES
+# include $(BUILD_SHARED_LIBRARY) # Will build it to .so library
