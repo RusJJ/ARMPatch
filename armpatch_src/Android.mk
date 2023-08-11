f7a60df4bd7c35b8fbd97d95d0de4366034f4dbb
晓梦大师/XMDS <2841824304@qq.com>
2023-08-11 15:27:48 +0800

    .mk

--- armpatch_src/Android.mk
+++ armpatch_src/Android.mk
@@ -13,13 +13,13 @@ endif
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := dobby
-LOCAL_SRC_FILES := AML_PrecompiledLibs/$(TARGET_ARCH_ABI)/libdobby.a
+LOCAL_MODULE := GlossHook
+LOCAL_SRC_FILES := AML_PrecompiledLibs/$(TARGET_ARCH_ABI)/libGlossHook.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
-LOCAL_SHARED_LIBRARIES := substrate dobby
+LOCAL_SHARED_LIBRARIES := substrate GlossHook
 LOCAL_MODULE    := armpatch
 LOCAL_SRC_FILES := ARMPatch.cpp
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG
@@ -30,8 +30,8 @@ LOCAL_C_INCLUDES += $(LOCAL_PATH)/xDL/xdl/src/main/cpp $(LOCAL_PATH)/xDL/xdl/src
 LOCAL_SRC_FILES += xDL/xdl/src/main/cpp/xdl.c xDL/xdl/src/main/cpp/xdl_iterate.c \
                    xDL/xdl/src/main/cpp/xdl_linker.c xDL/xdl/src/main/cpp/xdl_lzma.c \
                    xDL/xdl/src/main/cpp/xdl_util.c
-LOCAL_CFLAGS += -D__XDL -D__USEDOBBY
-LOCAL_CXXFLAGS += -D__XDL -D__USEDOBBY
+LOCAL_CFLAGS += -D__XDL -D__USE_GLOSSHOOK
+LOCAL_CXXFLAGS += -D__XDL -D__USE_GLOSSHOOK
 
 include $(BUILD_STATIC_LIBRARY) # Will build it to static .a library
 # include $(PREBUILT_SHARED_LIBRARY) # Can be used in LOCAL_SHARED_LIBRARIES
